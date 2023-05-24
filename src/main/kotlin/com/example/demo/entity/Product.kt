c95356aa6673eae25e27acd53694b71e82eec56f
tttokiooo <tt2316tokio@gmail.com>
2023-05-25 08:32:43 +0900

    値オブジェクトを使用

--- src/main/kotlin/com/example/demo/entity/Product.kt
+++ src/main/kotlin/com/example/demo/entity/Product.kt
@@ -1,15 +1,18 @@
 package com.example.demo.entity
 
+import com.example.demo.type.IdValue
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
+typealias ProductId = IdValue<Product>
+
 @Entity
 class Product(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long = 0,
+    var id: ProductId = ProductId(0),
     var name: String,
     var price: Int,
 )
---
+++ src/main/kotlin/com/example/demo/type/IdValue.kt
@@ -0,0 +1,4 @@
+package com.example.demo.type
+
+@JvmInline
+value class IdValue<T>(val value: Long)
